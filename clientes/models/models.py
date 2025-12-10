fc6877c2e032fa8538453e1141b8d32e6506f19c
alion1992 <55086590+alion1992@users.noreply.github.com>
2025-12-10 10:58:49 +0100

    commit clase 10/12/2025

--- clientes/models/models.py
+++ clientes/models/models.py
@@ -8,15 +8,33 @@ class Clientes(models.Model):
      _description = 'clientes.clientes'
 
      nombre = fields.Char()
-     apellido = fields.Char()
+     apellido = fields.Char(string='Primer Apellido')
+     apellido2 = fields.Char(string='Segundo Apellido')
      descripcion = fields.Text()
+     telefono = fields.Integer()
+     fecha_nacimiento = fields.Date()
+     activo = fields.Boolean()
+     puesto_id = fields.Many2one('clientes.puesto')
+     renta_id = fields.Many2one('clientes.renta')
+
+class Renta(models.Model):
+     _name = 'clientes.renta'
+     _description = 'clientes.renta'
+
+     codigo = fields.Char()
+     ano = fields.Integer(string='Año')
+     descripcion = fields.Char()
+     clientes_id = fields.One2many('clientes.clientes', 'renta_id')
+
 
 class Puesto(models.Model):
 
     _name = 'clientes.puesto'
-    _description = 'tipo puesto trabajo'
-
+    _description = 'Tipo puesto trabajo'
+    
+    nombre = fields.Char()
     descripcion = fields.Char()
+    clientes_id = fields.One2many('clientes.clientes', 'puesto_id')
 
 
 
